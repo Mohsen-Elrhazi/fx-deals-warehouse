43403fdab6be6fd24af2fa93963de2471fc5e64a
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2025-12-03 17:01:47 +0100

    Add ApiResponse DTO for standardized API responses

---
+++ src/main/java/com/fxdealswarehouse/dto/response/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.fxdealswarehouse.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ApiResponse <T>{
+    private String status;
+    private String message;
+    private T data;
+}
