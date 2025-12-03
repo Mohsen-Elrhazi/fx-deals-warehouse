54a575ef158638dc8dcc1a39d6d67d33ddbc9d8a
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2025-12-03 17:01:02 +0100

    Add DealRepository for CRUD operations on Deal entity

--- src/main/java/com/fxdealswarehouse/repository/DealRepository.java
+++ src/main/java/com/fxdealswarehouse/repository/DealRepository.java
@@ -2,6 +2,12 @@ package com.fxdealswarehouse.repository;
 
 import com.fxdealswarehouse.model.Deal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface DealRepository extends JpaRepository<Deal, String> {
+import java.util.Optional;
+
+@Repository
+public interface DealRepository extends JpaRepository<Deal, Long> {
+    boolean existsByDealId(String dealId);
+    Optional<Deal> findByDealId(String dealId);
 }
